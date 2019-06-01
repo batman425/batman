d09bf33b5ea9445cf6778462b0c2285718ac5297
batman425 <873001704@qq.com>
2019-06-01 23:31:51 +0800

    新增镜像打包文件

---
+++ gexin-docker-demo/src/main/java/com/batman/gexindockerdemo/GexinDockerDemoApplication.java
@@ -0,0 +1,14 @@
+package com.batman.gexindockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class GexinDockerDemoApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(GexinDockerDemoApplication.class, args);
+    }
+}

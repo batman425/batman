efe959f51ddb346deea9674347463a00a0b738f9
batman425 <873001704@qq.com>
2019-06-05 11:29:20 +0800

    优化配置文件

--- gexin-docker-demo/src/main/java/com/batman/gexindockerdemo/GexinDockerDemoApplication.java
+++ gexin-docker-demo/src/main/java/com/batman/gexindockerdemo/GexinDockerDemoApplication.java
@@ -4,11 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author liusongwei
+ * @Title: GexinDockerDemoApplication
+ * @ProjectName batman
+ * @Description: TODO
+ * @date 2019/6/510:59
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class GexinDockerDemoApplication {
     public static void main(String[] args) {
-
         SpringApplication.run(GexinDockerDemoApplication.class, args);
     }
 }
